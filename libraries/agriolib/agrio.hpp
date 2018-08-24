4e1412a6ec182c79c042380156c8f9da979095bc
root <root@DESKTOP-H6KC8VG.localdomain>
2018-08-24 16:21:31 +0300

    Initial commit

---
+++ libraries/agriolib/agrio.hpp
@@ -0,0 +1,11 @@
+/**
+ *  @file
+ *  @copyright defined in agr/LICENSE.txt
+ */
+#pragma once
+#include <agriolib/types.hpp>
+#include <agriolib/action.hpp>
+#include <agriolib/print.hpp>
+#include <agriolib/multi_index.hpp>
+#include <agriolib/dispatcher.hpp>
+#include <agriolib/contract.hpp>
